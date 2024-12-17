7b780cf45bc24b1c3227f5128fa0105da09fa887
DanielRamirez404 <danielramirezabou404@gmail.com>
2024-12-17 18:43:55 -0400

    added the SafeZone class

---
+++ Woolly-Breakout/src/safe-zone.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "map-utilities.h"
+#include <list>
+
+class SafeZone {
+    public:
+
+        void addKey(const Coordinates<int>& coordinates);
+        void pickKeyUp(const Coordinates<int>& coordinates);
+        void setDoor(const Coordinates<int>& coordinates);
+        bool isKey(const Coordinates<int>& coordinates);
+        bool isOpen() const;
+        const Coordinates<int>& getDoor() const;
+        const std::list<Coordinates<int>> getKeys() const; 
+
+    private:
+        Coordinates<int> doorCoordinates{};
+        std::list<Coordinates<int>> keys{};
+};
+
+
