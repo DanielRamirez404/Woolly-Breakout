071ee7a2371278ad40774bff8c20e3716edce4fb
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-05-25 01:57:27 -0400

    Add MenuWindow class

---
+++ Woolly-Breakout/src/game/ui/menuWindow.cpp
@@ -0,0 +1,54 @@
+#include "menuWindow.h"
+#include "elements.h"
+#include "window.h"
+#include "../../constants/constants.h"
+#include "SDL.h"
+#include <string>
+#include <algorithm>
+
+MenuWindow::MenuWindow() : Window() {
+	renderer.clear();
+    renderer.setArea(0, Constants::Window::size);
+    renderer.addTexture("menu-bg");
+}
+
+void MenuWindow::addButton(const std::string& texture, MenuWindow::Button button) {
+    renderer.setArea(button.x, button.y, button.width, button.length);
+    renderer.addTexture(texture); 
+    buttons.push_back(button);
+}
+
+void MenuWindow::run() {
+    SDL::Event event{};
+		
+	renderer.render();
+    
+    bool hasUserClicked{ false };
+    Button* clickedButton{ nullptr };
+
+    while (true) {
+        while (SDL_PollEvent(&event)) {
+
+            if (event.type == SDL_QUIT)
+				return;
+
+            if (event.type == SDL_MOUSEBUTTONDOWN) {
+                if (event.button.button == SDL_BUTTON_LEFT) 
+                    clickedButton = getClickedButton(event.button.x, event.button.y);
+            }
+            else if (event.type == SDL_MOUSEBUTTONUP) {
+                if (clickedButton)
+                    clickedButton->onClick();
+            }
+        }
+    }
+}
+
+MenuWindow::Button* MenuWindow::getClickedButton(int x, int y) {
+   auto foundButton { std::find_if(buttons.begin(), buttons.end(), [&](Button& button) {
+        return button.includes(x, y);
+    })}; 
+
+    return (foundButton != buttons.end()) ? &*foundButton : nullptr;
+}
+
---
+++ Woolly-Breakout/src/game/ui/menuWindow.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "window.h"
+#include <string>
+#include <vector>
+#include <functional>
+
+class MenuWindow : public Window {
+	public:
+        
+        struct Button {
+            int x{};
+            int y{};
+            int width{};
+            int length{};
+            std::function<void()> onClick{};
+            bool includes(int x_coor, int y_coor) { return (x_coor <= x + width && x_coor >= x) && (y_coor <= y + length && y_coor >= y); };
+        };
+
+        MenuWindow();
+        
+        void addButton(const std::string& texture, Button button);
+        
+        void run();
+
+    private:
+
+        std::vector<Button> buttons{};
+        Button* getClickedButton(int x, int y);
+};
