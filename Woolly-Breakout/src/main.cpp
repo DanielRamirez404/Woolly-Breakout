1f3c6a74fb19d7c8761794f44eda49561c8effd3
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-05-25 02:00:35 -0400

    Make test implementation of a menu window

--- Woolly-Breakout/src/main.cpp
+++ Woolly-Breakout/src/main.cpp
@@ -4,12 +4,21 @@
 #include <string>
 #include <utility>
 
+#include "game/ui/menuWindow.h"
+
 int main(int argc, char *argv[]) {
 
 	try {
 		std::string prompt{ (argc == 1) ? "" : argv[1] };
-		Game myGame{std::move(prompt)};
-		myGame.run();
+		MenuWindow w{};
+
+        w.addButton("grass", MenuWindow::Button{ 300, 300, 250, 100, [&](){ 
+            Game myGame{std::move(prompt)};
+            myGame.run(); 
+        } });
+        
+        w.run();
+
 	} catch(const std::runtime_error& error) {
 		std::cerr << "Runtime error: " << error.what() << ".\n";
 	} catch(...) {
